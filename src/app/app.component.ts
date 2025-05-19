6de6cc58cb0f10398e4b0de2ccb2090fabba1499
MirkanPolat <Mirkanpolat@outlook.de>
2025-05-19 21:52:11 +0200

    Initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,8 +2,9 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { MainContentComponent } from './main-content/main-content.component';
-import { FooterComponent } from './shared/footer.component';
-import { ImprintComponent } from './shared/imprint.component';
+import { FooterComponent } from './shared/components/footer/footer.component';
+import { ImprintComponent } from './imprint/imprint.component';
+
 
 @Component({
   selector: 'app-root',
